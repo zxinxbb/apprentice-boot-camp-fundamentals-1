399f5ecef26ef0ca1101c9200aee57398c17c78e
zainab gujar <zainab.gujar@autotrader.co.uk>
2025-03-11 16:13:28 +0000

    tidy up

--- exercises/java/src/main/java/cards/Cards.java
+++ exercises/java/src/main/java/cards/Cards.java
@@ -6,22 +6,22 @@ public class Cards {
     public static void main(String[] args) {
         Cards cards = new Cards();
         String[] deckInOrder = cards.getCards();
-        for(String card: deckInOrder){
+        for (String card : deckInOrder) {
             //System.out.println(card);
         }
     }
 
     public String[] getCards() {
         String[] result = new String[52];
-
-        PlayingCardDeck deck = new PlayingCardDeck();
-        deck = deck.createsOrderedDeck();
-
+        PlayingCardDeck deck = new PlayingCardDeck().createsOrderedDeck();
         int cardNumber = 0;
+
         for (PlayingCard card : deck.getPlayingCards()) {
             String faceValueName;
-            switch (card.getFaceValue()){
-                case 0: faceValueName = "ace"; break;
+            switch (card.getFaceValue()) {
+                case 0:
+                    faceValueName = "ace";
+                    break;
                 case 1:
                 case 2:
                 case 3:
@@ -30,11 +30,20 @@ public class Cards {
                 case 6:
                 case 7:
                 case 8:
-                case 9: faceValueName = Integer.toString(card.getFaceValue()+1); break;
-                case 10: faceValueName = "jack"; break;
-                case 11: faceValueName = "queen"; break;
-                case 12: faceValueName = "king"; break;
-                default: throw new IllegalArgumentException("Something went wrong " + card + "is not a valid faceValue!");
+                case 9:
+                    faceValueName = Integer.toString(card.getFaceValue() + 1);
+                    break;
+                case 10:
+                    faceValueName = "jack";
+                    break;
+                case 11:
+                    faceValueName = "queen";
+                    break;
+                case 12:
+                    faceValueName = "king";
+                    break;
+                default:
+                    throw new IllegalArgumentException("Something went wrong " + card + "is not a valid faceValue!");
             }
             result[cardNumber] = faceValueName + " of " + card.getSuit().getName();
             cardNumber++;
