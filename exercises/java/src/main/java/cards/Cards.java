96de349d446e203b84a95a3b4a93a08afa629373
zainab gujar <zainab.gujar@autotrader.co.uk>
2025-03-11 16:08:39 +0000

    refactors so creates deck in PlayingCardDeck

--- exercises/java/src/main/java/cards/Cards.java
+++ exercises/java/src/main/java/cards/Cards.java
@@ -1,31 +1,22 @@
 package cards;
 
+import java.util.Arrays;
+
 public class Cards {
     public static void main(String[] args) {
         Cards cards = new Cards();
         String[] deckInOrder = cards.getCards();
         for(String card: deckInOrder){
-            System.out.println(card);
+            //System.out.println(card);
         }
     }
 
     public String[] getCards() {
         String[] result = new String[52];
-        PlayingCardDeck deck = new PlayingCardDeck();
-        String suitName = "";
-        for (int suit = 0; suit < 4; suit++) {
-            for (int faceValue = 0; faceValue < 13; faceValue++) {
 
-                switch (suit){
-                    case 0: suitName = "clubs"; break;
-                    case 1: suitName = "diamonds"; break;
-                    case 2: suitName = "hearts"; break;
-                    case 3: suitName = "spades"; break;
-                }
-                Suit newSuit = new Suit(suitName);
-               deck.getPlayingCards()[suit*13+faceValue] = new PlayingCard(newSuit, faceValue);
-            }
-        }
+        PlayingCardDeck deck = new PlayingCardDeck();
+        deck = deck.createsOrderedDeck();
+
         int cardNumber = 0;
         for (PlayingCard card : deck.getPlayingCards()) {
             String faceValueName;
@@ -45,13 +36,9 @@ public class Cards {
                 case 12: faceValueName = "king"; break;
                 default: throw new IllegalArgumentException("Something went wrong " + card + "is not a valid faceValue!");
             }
-
-
-
             result[cardNumber] = faceValueName + " of " + card.getSuit().getName();
             cardNumber++;
         }
-
         return result;
     }
 }
--- exercises/java/src/main/java/cards/PlayingCardDeck.java
+++ exercises/java/src/main/java/cards/PlayingCardDeck.java
@@ -7,4 +7,31 @@ public class PlayingCardDeck {
     public PlayingCard[] getPlayingCards() {
         return playingCards;
     }
+
+    public PlayingCardDeck createsOrderedDeck() {
+        PlayingCardDeck deck = new PlayingCardDeck();
+        String suitName = "";
+        for (int suit = 0; suit < 4; suit++) {
+            for (int faceValue = 0; faceValue < 13; faceValue++) {
+
+                switch (suit) {
+                    case 0:
+                        suitName = "clubs";
+                        break;
+                    case 1:
+                        suitName = "diamonds";
+                        break;
+                    case 2:
+                        suitName = "hearts";
+                        break;
+                    case 3:
+                        suitName = "spades";
+                        break;
+                }
+                Suit newSuit = new Suit(suitName);
+                deck.getPlayingCards()[suit * 13 + faceValue] = new PlayingCard(newSuit, faceValue);
+            }
+        }
+        return deck;
+    }
 }
